249274f70aa0f4a8f83105da82783554c871821e
junaid <junaid@Junaids-Device>
2024-09-15 17:59:22 +0530

    updates

--- src/main/java/com/scheduler/dokcer_fo_mail/task/EmailSender.java
+++ src/main/java/com/scheduler/dokcer_fo_mail/task/EmailSender.java
@@ -2,6 +2,7 @@ package com.scheduler.dokcer_fo_mail.task;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,12 @@ import com.mailjet.client.resource.Emailv31;
 
 @Component
 public  class EmailSender {
+	
+	@Value("${email.send.waqf.from}")
+	private String fromEmail;
+	
+	@Value("${email.send.waqf.to}")
+	private String toEmail;
 	
 	@Async("mailAsync")
 	public  void sendEmailForOrderConfirmations(String emailId, String name, Double totalAmount) {
@@ -41,15 +48,18 @@ public  class EmailSender {
 		MailjetClient client;
 		MailjetRequest request;
 		MailjetResponse response;
+		System.out.println("From emailId is-->"+fromEmail);
+		System.out.println("To emailId is-->"+toEmail);
+
 		client = new MailjetClient("3e292e1e3e850abe850793dbb22554b9",
 				"2fa15000afb8c7ad2cd676c9828bcd5e", new ClientOptions("v3.1"));
 		request = new MailjetRequest(Emailv31.resource).property(Emailv31.MESSAGES,
 				new JSONArray().put(new JSONObject()
-						.put(Emailv31.Message.FROM, new JSONObject().put("Email", "saim@friendsmobile.store")
+						.put(Emailv31.Message.FROM, new JSONObject().put("Email", fromEmail)
 								.put("Name", "Reject Waqf Amendment Bill"))
 						.put(Emailv31.Message.TO,
 								new JSONArray().put(
-										new JSONObject().put("Email", "jpcwaqf-lss@sansad.nic.in").put("JPC Waqf Board", "Hello")))
+										new JSONObject().put("Email", toEmail).put("JPC Waqf Board", "Hello")))
 						.put(Emailv31.Message.SUBJECT, "Complete Rejection of the Waqf Amendment Bill, 2024.")
 						.put(Emailv31.Message.TEXTPART, "Dear Members of the Joint Parliamentary Committee,\n"
 								+ "I strongly oppose the Waqf Amendment Bill. It violates fundamental rights, Islamic charity laws, and established judicial principles while introducing significant operational inefficiencies.\n"
